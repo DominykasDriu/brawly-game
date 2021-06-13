3dc27ab07b25e301739c70b8fc56950b7b251de3
DominykasDriu <dominykas.driukas@gmail.com>
2021-06-13 14:20:51 +0300

    add context provider & header to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import Header from './compoenents/Header'
 import Arena from './pages/Arena'
 import InventoryPage from './pages/InventoryPage'
 import Leaderboard from './pages/Leaderboard'
@@ -12,36 +14,42 @@ import Shop from './pages/Shop'
 import Profile from './pages/Profile'
 import Signup from './pages/Signup'
 
+export const UserContext = React.createContext('')
+
 function App() {
+  const [user, setUser] = useState(null)
   return (
-    <Router>
-      <Switch>
-        <Route path='/menu'>
-          <Menu/>
-        </Route>
-        <Route path='/arena'>
-          <Arena/>
-        </Route>
-        <Route path='/inventory'>
-          <InventoryPage/>
-        </Route>
-        <Route path='/leaderboard'>
-          <Leaderboard/>
-        </Route>
-        <Route path='/login'>
-          <Login/>
-        </Route>
-        <Route path='/signup'>
-          <Signup/>
-        </Route>
-        <Route path='/shop'>
-          <Shop/>
-        </Route>
-        <Route path='/profile/:name'>
-          <Profile/>
-        </Route>
-      </Switch>
-    </Router>
+    <UserContext.Provider value={{user, setUser}}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/menu'>
+            <Menu/>
+          </Route>
+          <Route path='/arena'>
+            <Arena/>
+          </Route>
+          <Route path='/inventory'>
+            <InventoryPage/>
+          </Route>
+          <Route path='/leaderboard'>
+            <Leaderboard/>
+          </Route>
+          <Route path='/login'>
+            <Login/>
+          </Route>
+          <Route path='/signup'>
+            <Signup/>
+          </Route>
+          <Route path='/shop'>
+            <Shop/>
+          </Route>
+          <Route path='/profile/:name'>
+            <Profile/>
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
