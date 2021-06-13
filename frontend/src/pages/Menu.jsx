b2414560a29c05c1355759f9717fbe08ba49934c
DominykasDriu <dominykas.driukas@gmail.com>
2021-06-13 16:00:09 +0300

    add Menu logic

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from "react-router-dom";
+import { UserContext } from '../App';
 
 export default function Menu() {
-  return (
-    <div>
+  const data = useContext(UserContext)
+  let user = data.user.user || null
 
-    </div>
+  return (
+    <main>
+      {user &&
+      <div>
+        <img src={user.image} alt="profile-img" />
+        <p>Name: {user.username}</p>
+        <p>Health: {user.health}</p>
+        <p>Gold: {user.gold}</p>
+      </div>
+      }
+      <div className="menu">
+        <Link to="/inventory">Inventory</Link>
+        <Link to="/shop">Shop</Link>
+        <Link to="/leaderboard">Leaderboard</Link>
+        <Link to="/arena">Arena</Link>
+      </div>
+    </main>
   )
 }
