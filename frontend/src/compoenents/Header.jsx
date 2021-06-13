8e2666bc3b8788fa07d0c7f03bbfb028d71bc93f
DominykasDriu <dominykas.driukas@gmail.com>
2021-06-13 14:42:51 +0300

    add header log in/out logic

--- frontend/src/compoenents/Header.jsx
+++ frontend/src/compoenents/Header.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import { UserContext } from '../App';
 
 export default function Header() {
+  let history = useHistory()
+  const user = useContext(UserContext)
+  
+  const handleLogOut = () => {
+    user.setUser(null)
+    history.push('/leaderboard')
+  }
+
   return (
     <header>
-
+      <h1>Brawly</h1>
+      {!user.user ? 
+      <div>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/login">Log In</Link>
+      </div>
+      :
+      <button onClick={handleLogOut}>Log Out</button>
+      }
     </header>
   )
 }
