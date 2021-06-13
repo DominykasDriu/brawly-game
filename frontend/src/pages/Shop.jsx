e22cd0657dff96cbfe0971bf3639ef225fc1a58b
DominykasDriu <dominykas.driukas@gmail.com>
2021-06-13 21:39:18 +0300

    fix to reflect bc changes to buy/sell

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -13,17 +13,15 @@ export default function Shop() {
     .then(data => setItems(data))
   }, [])
 
-  const buyItem = (type, level, price) => {
+  const buyItem = (item) => {
+    let id = String(Date.now()) + String(Math.floor(Math.random() * 1000));
     fetch('http://localhost:3001/api/buy', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
         'token': userState.user.token
       },
-      body: JSON.stringify({
-        item: {type, level},
-        price
-      })
+      body: JSON.stringify({...item, id})
     })
     .then(res => res.json())
     .then(data => {
@@ -37,15 +35,16 @@ export default function Shop() {
 
   return (
     <main>
+      <h1>Your gold: {userState.user.user.gold}</h1>
       <div className="shop-items">
         <div className="armors">
           {items && items.armors.map((e, index) => (
-            <div key={index} style={{display: userState.user.user.inventory.armors.includes(e.level) && 'none'}}>
+            <div key={index}>
               <p>{e.name}</p>
               <p>Price: {e.price}$</p>
               <button 
               disabled={userState.user.user.gold <= e.price}
-              onClick={() => buyItem(e.type, e.level, e.price)}
+              onClick={() => buyItem(e)}
               >BUY</button>
             </div>
           ))}
@@ -57,7 +56,7 @@ export default function Shop() {
               <p>Price: {e.price}$</p>
               <button 
               disabled={userState.user.user.gold <= e.price}
-              onClick={() => buyItem(e.type, e.level, e.price)}
+              onClick={() => buyItem(e)}
               >BUY</button>
             </div>
           ))}
@@ -69,7 +68,7 @@ export default function Shop() {
               <p>Price: {e.price}$</p>
               <button 
               disabled={userState.user.user.gold <= e.price}
-              onClick={() => buyItem(e.type, e.level, e.price)}
+              onClick={() => buyItem(e)}
               >BUY</button>
             </div>
           ))}
