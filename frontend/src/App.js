9671d9dde05c65546be39253a12d3c71a467f49a
DominykasDriu <dominykas.driukas@gmail.com>
2021-06-13 13:01:19 +0300

    add pages & router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,47 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Arena from './pages/Arena'
+import InventoryPage from './pages/InventoryPage'
+import Leaderboard from './pages/Leaderboard'
+import Login from './pages/Login'
+import Menu from './pages/Menu'
+import Shop from './pages/Shop'
+import Profile from './pages/Profile'
+import Signup from './pages/Signup'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/menu'>
+          <Menu/>
+        </Route>
+        <Route path='/arena'>
+          <Arena/>
+        </Route>
+        <Route path='/inventory'>
+          <InventoryPage/>
+        </Route>
+        <Route path='/leaderboard'>
+          <Leaderboard/>
+        </Route>
+        <Route path='/login'>
+          <Login/>
+        </Route>
+        <Route path='/signup'>
+          <Signup/>
+        </Route>
+        <Route path='/shop'>
+          <Shop/>
+        </Route>
+        <Route path='/profile/:name'>
+          <Profile/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
