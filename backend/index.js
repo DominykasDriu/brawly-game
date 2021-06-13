3400ded4ebbe530babfcc3419bd8bcc68b7e8b4a
DominykasDriu <dominykas.driukas@gmail.com>
2021-06-13 13:42:38 +0300

    fix backend cors req

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
 const routes = require('./routes/routes')
 const PORT = process.env.PORT || 3000
 const DB = process.env.DB_PATH
-// const cors = require('cors')
 
 mongoose.connect(DB, {
   useNewUrlParser: true,
@@ -15,11 +15,6 @@ mongoose.connect(DB, {
 })
 .then(res => app.listen(PORT))
 
-// Cors options
-// const corsOptions = {
-//   exposedHeaders: ['token']
-// }
-
-// app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 app.use('/api', routes)
