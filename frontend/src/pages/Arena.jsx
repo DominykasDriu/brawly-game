4742a10429e5f37adbf24d75e34e323218ac4d1c
DominykasDriu <dominykas.driukas@gmail.com>
2021-06-15 20:53:58 +0300

    add more Arena logic

--- frontend/src/pages/Arena.jsx
+++ frontend/src/pages/Arena.jsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState, useContext } from 'react'
 import { Link } from "react-router-dom";
 import { UserContext } from '../App';
 import HealthBar from '../compoenents/HealthBar';
+import ItemCard from '../compoenents/ItemCard';
 
 export default function Arena() {
   const userState = useContext(UserContext)
 
   const [monsters, setMonsters] = useState(null)
   const [currentMonster, setCurrentMonster] = useState({})
+  const [selectedArmor, setSelectedArmor] = useState(null)
+  const [selectedWeapon, setSelectedWeapon] = useState(null)
 
   useEffect(() => {
     fetch('monsters.json')
@@ -23,6 +26,35 @@ export default function Arena() {
     if (monsters) {
       let rnd = Math.floor(Math.random() * 3)
       setCurrentMonster({...monsters[rnd], hp: 100})
+    }
+  }
+
+  const updateHealth = (action, val, item) => {
+    fetch(`http://localhost:3001/api/health/${action}/${val}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'token': userState.token
+      },
+      body: JSON.parse({id: item.id || null})
+    })
+    .then(res => res.json())
+    .then(data => {
+      if (data.success) {
+        userState.setUser(prevState => {return {...prevState, user: data.user}})
+      } else {
+        console.log(data)
+      }
+    })
+  }
+
+  const useItem = (item) => {
+    if (item.type === 'potion') {
+      updateHealth('add', item.stat, item)
+    } else if (item.type === 'armor') {
+      setSelectedArmor(<ItemCard {...item} btn={null}/>)
+    } else {
+      setSelectedWeapon(<ItemCard {...item} btn={null}/>)
     }
   }
 
@@ -48,8 +80,23 @@ export default function Arena() {
         </div>
       </div>
       <button>Attack</button>
-      <div className="inventory">
-        
+      <div className="items">
+        <div className="items_current">
+          <div className="items_current__armor">
+            <h3>Armor</h3>
+            {selectedArmor}
+          </div>
+          <div className="items_current__weapon">
+            <h3>Weapons</h3>
+            {selectedWeapon}
+          </div>
+        </div>
+        <div className="items_inventory">
+          inventory:
+          {userState.user.inventory.map(e => (
+            <ItemCard {...e} btn={'Use'} fnc={useItem} key={e.id}/>
+          ))}
+        </div>
       </div>
       <Link to="/menu">Run Away!</Link>
     </main>
