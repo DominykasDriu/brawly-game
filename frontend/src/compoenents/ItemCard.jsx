c735487d594cab72b63bf230bf069e29f02b6649
DominykasDriu <dominykas.driukas@gmail.com>
2021-06-20 12:42:41 +0300

    add ItemCard component

--- frontend/src/compoenents/ItemCard.jsx
+++ frontend/src/compoenents/ItemCard.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import ReactTooltip from 'react-tooltip';
 
 export default function ItemCard(item) {
 
   return (
-    <div className="item-card">
-      <img src={item.image} alt="" />
-      <h3>{item.name}</h3>
-      <p>Lvl. {item.level} {item.type}</p>
+    <div className="item-card" data-tip={item.description}>
+      <img src={item.image} alt="item"/>
+      <p className="item-card_name">{item.name}</p>
+      <p className="item-card_level">Lvl. {item.level} {item.type}</p>
       {/* If button text is passed render it */}
-      {item.btn && <button onClick={() => item.fnc(item)}>{item.btn}</button>}
+      {item.btn && <button className="btn small" disabled={item.disabled} onClick={() => item.fnc(item)}>{item.btn}</button>}
+      <ReactTooltip/>
     </div>
   )
 }
