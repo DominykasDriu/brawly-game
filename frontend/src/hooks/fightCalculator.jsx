ed193d4161e7fe17c475b18872922bbfee96d3e7
DominykasDriu <dominykas.driukas@gmail.com>
2021-06-20 12:44:03 +0300

    add fight calculator hook

--- frontend/src/hooks/fightCalculator.jsx
+++ frontend/src/hooks/fightCalculator.jsx
@@ -1,17 +1,18 @@
 
 export default function fightCalculator(monster, armors, weapons) {
+  console.log(weapons);
   const random = (max) => {
     return Math.floor(Math.random() * max) + 1
   }
   
-  let userDmg = random(weapons.props.stat)
-  let userDef = random(armors.props.stat)
+  let userDmg = random(weapons.stat)
+  let userDef = random(armors.stat)
   let userHeal = false
   let userLoot = random(11) - 1
   let monsterDmg = random(monster.damage)
-  if (weapons.props.level === 1 && random(100) > 80) monsterDmg = 0
-  if (weapons.props.level === 2 && random(100) > 70) userDmg += userDmg
-  if (weapons.props.level === 3 && random(100) > 60) userHeal = true
+  if (weapons.level === 1 && random(100) > 80) monsterDmg = 0
+  if (weapons.level === 2 && random(100) > 70) userDmg += userDmg
+  if (weapons.level === 3 && random(100) > 60) userHeal = true
 
   return {
     monsterDmg: monsterDmg <= userDef ? 0 : monsterDmg - userDef,
