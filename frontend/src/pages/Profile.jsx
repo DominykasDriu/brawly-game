ed0466ec97e9edbb9713e2dfd5b6315da550e412
DominykasDriu <dominykas.driukas@gmail.com>
2021-06-20 12:42:20 +0300

    add Profile styles & logic

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -14,23 +14,28 @@ export default function Profile() {
 
   return (
     <main>
-      {profile &&
-       <div>
-          <div className="profile-info">
-            <img src={profile.image}/>
-            <h1>Name: {profile.username}</h1>
-            <p>Health: {profile.health}</p>
-            <p>Gold: {profile.gold}</p>
+      <div className="container profile">
+        <h2>{profile && profile.username}</h2>
+        {profile &&
+        <div className="profile_wrapper">
+          <div className="user-stats">
+            <div className="user-stats_image profile-image">
+              <img src={profile.image} alt="profile-img" />
+            </div>
+            <div className="user-stats_text-container">
+              <p className="user-stats_text-container__text"><img src="https://i.ibb.co/MGMrp82/heart.png"/> {profile.health}</p>
+              <p className="user-stats_text-container__text"><img src="https://i.ibb.co/6yRysPV/coin.png"/> {profile.gold}</p>
+            </div>
           </div>
-          <div className="inventory">
-            <p>User items:</p>
+          <div className={'inventory ' + (profile.inventory.length >= 10 && 'scroll w-btn')}>
             {profile.inventory.map(e => (
-              <ItemCard {...e} key={e.id}/>
+              <ItemCard {...e} key={e.id} btn={null}/>
             ))}
           </div>
-       </div>
-      }
-      <Link to="/leaderboard">Back to Leaderboard</Link>
+        </div>
+        }
+        <Link className="btn" to="/leaderboard">Back to Leaderboard</Link>
+      </div>
     </main>
   )
 }
